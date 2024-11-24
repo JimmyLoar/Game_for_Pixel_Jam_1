5e1227b641bf813cec382025ff2a08a5958e5fef
JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
2024-11-24 15:34:26 +0300

    last update

--- core/screen/click_zone/click_area.gd
+++ core/screen/click_zone/click_area.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal clicked
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
 
 func _ready() -> void:
 	if get_parent() is SubViewport:
@@ -17,4 +18,6 @@ func _update_sprite_position():
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_pressed() and event is InputEventMouseButton:
 		clicked.emit()
+		gpu_particles_2d.position = get_global_mouse_position()
+		gpu_particles_2d.restart()
 		$AudioStreamPlayer.play_sound()
